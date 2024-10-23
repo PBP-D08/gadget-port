e05ea5bb7fde4a4b5430b7e65c2b76b366727320
MichelineWL <michelinewijaya@gmail.com>
2024-10-23 22:40:24 +0700

    feat: connect sign in page to navbar signin button

--- authentication/models.py
+++ authentication/models.py
@@ -6,10 +6,10 @@ class Profile(models.Model):
     # ROLE_CHOICES = [('admin','Admin'), ('auyer','Buyer')]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=5)
-    username = models.CharField(max_length=30)
+    username = models.CharField(max_length=31)
     full_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=200)
-    alamat = models.CharField(max_length=255)
+    alamat = models.CharField(max_length=255) 
 
     def is_admin(self):
         return self.role == 'Admin'
