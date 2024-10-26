d548bad612ac6567031442f46364be3cbdd4d367
albyarto <albyarto.07@gmail.com>
2024-10-26 13:14:42 +0700

    style: styling page html untuk app store

--- store/urls.py
+++ store/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('list_store/', views.list_store, name='list_store'),
     path('add_store/', views.add_store, name='add_store'),
     path('store_detail/<uuid:id>', views.store_detail, name='store_detail'),
-    path('delete/<uuid:id>', views.delete_store, name='delete_store'),
-    path('edit/<uuid:id>', views.edit_store, name='edit_store'),
+    path('delete_store/<uuid:id>', views.delete_store, name='delete_store'),
+    path('edit_store/<uuid:id>', views.edit_store, name='edit_store'),
 ]
