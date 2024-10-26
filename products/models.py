0de2fefe2cfa6f58e99b935d6bd4f6d8164f4ac9
albyarto <albyarto.07@gmail.com>
2024-10-26 18:41:15 +0700

    style: styling page html products dan menambah models

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User 
+from store.models import Store
 
 # Create your models here.
     
@@ -11,6 +12,7 @@ class Katalog(models.Model):
     price = models.PositiveIntegerField()
     image_link = models.URLField()
     spec = models.CharField(max_length=512)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name} - {self.brand}"
