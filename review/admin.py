b968593bc7e0dd09ef514d300dd55eb6d940adbc
Faridhsbi <mhmmd.faridhsbi@gmail.com>
2024-10-27 12:43:54 +0700

    fix: review card dan button

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display=('user', 'product', 'review_text', 'rating', 'timestamp')
+admin.site.register(Review, ProductReviewAdmin)
