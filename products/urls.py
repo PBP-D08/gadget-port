c69d282207e7fbae7ca4b02f4b209c02967dd80b
Faridhsbi <mhmmd.faridhsbi@gmail.com>
2024-10-27 23:43:38 +0700

    finalisasi

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ app_name = 'products'
 urlpatterns = [
     path('show_products/', views.show_products, name='show_products'),
     path('add_product/', views.add_product, name='add_product'),
+    path('delete_product/<int:id>', views.delete_product, name='delete_product'),
 ]
